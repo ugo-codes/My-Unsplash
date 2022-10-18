3007447dfc32bf02ef20c0cd3c315ebb10adb7f6
Ikedinma Ugochukwu <yourfavouritedeveloper@gmail.com>
2022-10-18 04:27:08 +0100

    solve the bug associated with the this keyword in the express middleware function

--- routes/photoRoutes.js
+++ routes/photoRoutes.js
@@ -4,7 +4,7 @@ const { photoController } = require('../controllers');
 const {authenticate} = require("../authentication")
 
 router.get('/photo/all', authenticate.authenticateApp.bind(authenticate), photoController.getAllPhotos);
-router.post('/photo/add', authenticate.authenticateApp, photoController.postPhoto);
-router.delete("/photo/delete", authenticate.authenticateApp, photoController.deletePhoto)
+router.post('/photo/add', authenticate.authenticateApp.bind(authenticate), photoController.postPhoto);
+router.delete("/photo/delete", authenticate.authenticateApp.bind(authenticate), photoController.deletePhoto)
 
 module.exports = router;
